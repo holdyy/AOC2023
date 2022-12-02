31af82ae874cc07d8453c5dd00fd6c8d44e13ea8
Holdyy <alexdtholdsworth@gmail.com>
2022-12-02 11:21:16 +0000

    day1

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,7 +1,21 @@
 export const day1 = (data: number[]) => {
+  var array = []
   let rob = 0
+  let bob = 0
+
   for (let i = 0; i < data.length; i++) {
-    console.log(i)
+    if (data[i] > 0) { rob += data[i] }
+    else { { array.push(rob) } { rob = 0 } }
+
   };
-  return rob;
+
+  let sorted = array.sort((a, b) => b - a);
+
+  for (let i = 0; i < 3; i++) {
+    console.log(sorted[i]), bob += sorted[i]
+  }
+
+  //  const max = array.reduce((a, b) => { return Math.max(a, b) });
+
+  return bob;
 };
