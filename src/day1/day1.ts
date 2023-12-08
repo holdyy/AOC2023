c19fbd7fda72a79fe254929572945fa7b7877910
Holdyy <alexdtholdsworth@gmail.com>
2023-12-08 18:30:41 +0000

    optimised day 1 part 1

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,19 +1,14 @@
 export const day1 = (data: string) => {
+  let numbers = "one, two, three, four, five, six, seven, eight, nine"
   let NumberArray = []
-  let FirstNumber = []
-  let LastNumber = []
   let Round = []
   let Number = 0
   let score = 0
-  for (let i = 0; i < data.length; i++) {
-    NumberArray = data[i].match(/\d+/g);
-    console.log(NumberArray)
-    FirstNumber = NumberArray[0].split('')
-    LastNumber = NumberArray[NumberArray.length - 1].split('')
-    //console.log(FirstNumber)
-    //console.log(LastNumber)
 
-    Round = FirstNumber[0] + LastNumber[LastNumber.length - 1]
+  for (let i = 0; i < data.length; i++) {
+    NumberArray = data[i].match(/\d/g);
+
+    Round = NumberArray[0] + NumberArray[NumberArray.length - 1]
     Number = +Round
     score += Number
 
